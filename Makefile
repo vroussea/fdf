183a0a718363ce42d4d9abb517cf53f3fe207fc8
Valentin ROUSSEAU <vroussea@e1r11p16.42.fr>
2016-03-02 22:59:31 +0100

    now read file and init map

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: vroussea <vroussea@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/11/24 09:55:27 by vroussea          #+#    #+#              #
-#    Updated: 2016/03/02 17:22:17 by vroussea         ###   ########.fr        #
+#    Updated: 2016/03/02 20:34:22 by vroussea         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,17 +19,17 @@ SOURCES =	main.c file_reader.c
 OBJS =		$(SOURCES:.c=.o)
 
 all :		$(NAME)
-$(NAME) :	$(LIB)
+$(NAME) :	lib
 			$(CC) $(CFLAGS) -c $(SOURCES)
 			$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(INCLUDES) -lmlx $(FRAMEW)
-$(LIB) :
+lib :
 			make -C libft/ re
 norm :
 			norminette $(SOURCES) libft.h
 meteo :
 			curl http://wttr.in/Paris
 clean :
-			-rm -f $(OBJS)
+			-rm -f $(OBJS) libft/*.o libft/*.a
 fclean :	clean
 			-rm -f $(NAME)
 re :		fclean all
--- file_reader.c
+++ file_reader.c
@@ -6,36 +6,67 @@
 /*   By: vroussea <vroussea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/02 16:21:57 by vroussea          #+#    #+#             */
-/*   Updated: 2016/03/02 18:56:04 by vroussea         ###   ########.fr       */
+/*   Updated: 2016/03/02 22:54:37 by vroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include "fdf.h"
 
-static int	init_map(int ***map, char *file)
+static int	sizer_x(char *file)
 {
-	int		i;
-	int		ret;
+	int	size_x;
+	char *line;
 	int		fd;
-	char	*line;
-	int		size;
+	int		ret;
 
 	fd = open(file, O_RDONLY);
 	if ((ret = get_next_line(fd, &line)) == -1)
 		return (0);
-	size = ft_strlen(line);
-	if (!(*map = (int **)ft_memalloc(sizeof(int *) * size)))
+	size_x = ft_strlen(line);
+	close(fd);
+	return (size_x);
+}
+
+static int	sizer_y(char *file)
+{
+	int		size_y;
+	char 	*line;
+	int		fd;
+	int		ret;
+
+	fd = open(file, O_RDONLY);
+	size_y = 0;
+	while ((ret = get_next_line(fd, &line)) > 0)
+		size_y++;
+	if (ret == -1)
+		return (0);
+	close(fd);
+	return (size_y);
+}
+
+static int	init_map(int ***map, char *file)
+{
+	int		i;
+	int		size_x;
+	int		size_y;
+
+	size_x = sizer_x(file);
+	size_y = sizer_y(file);
+	ft_putstr("size_x : ");
+	ft_putendl(ft_itoa(size_x));
+	ft_putstr("size_y : ");
+	ft_putendl(ft_itoa(size_y));
+	if (!(*map = (int **)ft_memalloc(sizeof(int *) * size_y)))
 		return (0);
 	i = 0;
-	while (i < size)
+	while (i < size_y)
 	{
-		if (!(*map[i] = (int *)ft_memalloc(sizeof(int) * size)))
+		if (!((*map)[i] = (int *)ft_memalloc(sizeof(int) * size_x)))
 			return (0);
 		i++;
 	}
-	close(fd);
-	return (size);
+	return (1);
 }
 
 int			file_reader(char *file, int ***map)
@@ -43,9 +74,11 @@ int			file_reader(char *file, int ***map)
 	int		fd;
 	int		ret;
 	char	*line;
-	int		size;
 
-	if ((size = init_map(map, file)) == 0)
+	if (!(size = init_map(map, file)))
+		return (0);
+	ft_putendl("mdr2");
+	if (!test_file(file))
 		return (0);
 	fd = open(file, O_RDONLY);
 	while ((ret = get_next_line(fd, &line)) > 0)
