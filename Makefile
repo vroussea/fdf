e8e15f901e2e7399252c5b12de59a44e9fe3ea69
Valentin ROUSSEAU <vroussea@e3r2p15.42.fr>
2016-03-17 17:16:07 +0100

    clean Makefile, add image_draw.c

--- Makefile
+++ Makefile
@@ -6,13 +6,14 @@
 #    By: vroussea <vroussea@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/11/24 09:55:27 by vroussea          #+#    #+#              #
-#    Updated: 2016/03/09 17:24:14 by vroussea         ###   ########.fr        #
+#    Updated: 2016/03/17 17:06:55 by vroussea         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME =		fdf
-INCLUDES =libft/libft.a
-FRAMEW =	-framework OpenGL -framework AppKit
+INCLUDES =	libft/libft.a
+MKINC =		make -C libft/
+MLX =		-lmlx -framework OpenGL -framework AppKit
 CC =		gcc
 CFLAGS =	-Wall -Wextra -Werror
 SOURCES =	main.c file_reader.c
@@ -20,10 +21,9 @@ OBJS =		$(SOURCES:.c=.o)
 
 all :		$(NAME)
 $(NAME) :
+			$(MKINC)
 			$(CC) $(CFLAGS) -c $(SOURCES)
-			$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(INCLUDES) -lmlx $(FRAMEW)
-lib :
-			make -C libft/ re
+			$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(INCLUDES) $(MLX)
 norm :
 			norminette $(SOURCES)
 meteo :
@@ -31,5 +31,6 @@ meteo :
 clean :
 			-rm -f $(OBJS)
 fclean :	clean
+			$(MKINC) fclean
 			-rm -f $(NAME)
 re :		fclean all
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vroussea <vroussea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/23 14:28:45 by vroussea          #+#    #+#             */
-/*   Updated: 2016/03/09 22:04:18 by vroussea         ###   ########.fr       */
+/*   Updated: 2016/03/17 17:13:06 by vroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,46 +54,37 @@ int	main(int argc, char **argv)
 
 	map = NULL;
 	if (argc == 2)
+	{
 		file_reader(argv[1], &map);
-	i = 0;
-	while (map[i] != NULL)
-	{
-		j = 0;
-		while (j < map[i][0] + 1)
+		i = 0;
+		while (map[i] != NULL)
 		{
-			ft_putnbr(map[i][j]);
-			j++;
-			if (map[i][j] < 10)
+			j = 0;
+			while (j < map[i][0] + 1)
+			{
+				ft_putnbr(map[i][j]);
+				j++;
+				if (map[i][j] < 10)
+					ft_putchar(' ');
 				ft_putchar(' ');
-			ft_putchar(' ');
+			}
+			ft_putchar('\n');
+			i++;
 		}
-		ft_putchar('\n');
-		i++;
+		x = 2560;
+		y = 1350;
+		if (!(mlx = mlx_init()))
+			return (0);
+		win = mlx_new_window(mlx, x, y, "test");
+		img = mlx_new_image(mlx, x, y);
+		memloc = mlx_get_data_addr(img, &bits_per_pixel, &size_line, &endian);
+		ft_putstr("size_line : ");
+		ft_putendl(ft_itoa(size_line));
+		draw_image(memloc, x, y);
+		mlx_put_image_to_window(mlx,win, img, 1, 1);
+		if ((mlx_key_hook(win, my_key_func, 0)) == 0)
+			return (0);
+		mlx_loop(mlx);
 	}
-	x = 500;
-	y = 500;
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, x, y, "test");
-	img = mlx_new_image(mlx, x, y);
-	memloc = mlx_get_data_addr(img, &bits_per_pixel, &size_line, &endian);
-	draw_image(memloc, x, y);
-	mlx_put_image_to_window(mlx,win, img, 1, 1);
-	/*while (x <= 450 && y <= 450)
-	{
-		mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
-		x++;
-		y++;
-	}
-	x = 50;
-	y = 450;
-	while (x <= 450 && y >= 50)
-	{
-		mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
-		x++;
-		y--;
-	}*/
-	if ((mlx_key_hook(win, my_key_func, 0)) == 0)
-		return (0);
-	mlx_loop(mlx);
 	return (1);
 }
